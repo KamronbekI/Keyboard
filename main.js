20a4955ed346adcaa7cd61f7a3f59f44b8f955f1
Farrukh I <farrukhi@farrukhs-mbp-2.gateway.sonic.net>
2021-07-27 21:43:57 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const keysAudio = document.querySelector(".btn");
+let soundClick = document.querySelector("#audio");
+
+keysAudio.addEventListener("click", () => {
+	soundClick.play();
+});
+
+const keySecond = document.querySelector(".button");
+let soundSecond = document.querySelector("#second");
+
+keySecond.addEventListener("click", () => {
+	soundSecond.play();
+});
+
+const keyThird = document.querySelector(".button2");
+let soundThird = document.querySelector("#third");
+
+keyThird.addEventListener("click", () => {
+	soundThird.play();
+});
+
+const keyForth = document.querySelector(".button3");
+let soundForth = document.querySelector("#forth");
+
+keyForth.addEventListener("click", () => {
+	soundForth.play();
+});
+
+const keyFifth = document.querySelector(".button4");
+let soundFifth = document.querySelector("#fifth");
+
+keyFifth.addEventListener("click", () => {
+	soundFifth.play();
+});
+
+const keySixth = document.querySelector(".button5");
+let soundSixth = document.querySelector("#sixth");
+
+keySixth.addEventListener("click", () => {
+	soundSixth.play();
+});
+const keyseventh = document.querySelector(".btn6");
+let soundseventh = document.querySelector("#seventh");
+
+keyseventh.addEventListener("click", () => {
+	soundseventh.play();
+});
+
+const keyeth = document.querySelector(".btn7");
+let soundeth = document.querySelector("#eth");
+
+keyeth.addEventListener("click", () => {
+	soundeth.play();
+});
